6ba46c685f55e4b824a57703426fb28d9ebeab86
toodos <avi449156@gmail.com>
2026-03-18 14:28:17 +0530

    Fix undefined rank/leaderboardStr in balance command

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -11,6 +11,15 @@ module.exports = {
         const userData   = db.getUser(targetUser.id);
         const allUsers   = db.getAllUsers(); // already sorted DESC by balance
 
+        const trophies = ['🥇', '🥈', '🥉'];
+        const top3 = allUsers.slice(0, 3);
+        const rank = allUsers.findIndex(u => u.userId === targetUser.id) + 1;
+
+        const leaderboardStr = top3
+            .map((u, i) => `${trophies[i]} <@${u.userId}>: **₹${u.balance.toLocaleString()}**`)
+            .join('\n')
+            + (rank > 3 ? `\n\n...You are at **#${rank}**` : '');
+
         const totalEconomy = allUsers.reduce((sum, u) => sum + u.balance, 0);
 
         const { file, embed } = balanceEmbed(interaction.client, targetUser, userData.balance, rank, leaderboardStr, totalEconomy);
